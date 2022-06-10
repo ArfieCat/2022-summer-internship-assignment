96d8077e1bd6d03e48635c487cd6eaa53d93400d
ArfieCat <arthur.guo.canada@gmail.com>
2022-06-10 01:51:11 -0700

    Complete Challenge 1: Algorithms

---
+++ algorithms/FindPairs.java
@@ -0,0 +1,66 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
+/**
+ * Question 1:
+ * 
+ * Find all pairs for a given sum.
+ * Create a function that accepts an array of integers and a target sum.
+ * The function will print all pairs of integers in the array whose sum is equal to the target sum.
+ */
+
+class FindPairs 
+{
+    public static void main(String[] args) 
+    {
+		// Test cases go here.
+	}
+
+    /**
+     * Approach:
+     * 
+     * 1. Remove duplicates from `testArray`.
+     *     a. Edge case: if `testArray` has less than 2 unique items, there are no pairs.
+     * 
+     * 2. Iterate through `testArray`, using a HashMap to track potential ("have x, need y") pairs.
+     *     a. If the current int was already needed, print the pair.
+     *     b. Otherwise, calculate the difference needed to reach `targetSum` and put the potential
+     *        pair in the HashMap.
+     */
+
+    static void findPairs(int[] testArray, int targetSum)
+    {
+        // Remove duplicates from `testArray`. (Stream: Java 8+)
+        testArray = Arrays.stream(testArray).distinct().toArray();
+
+        // If length is less than 2, there are no pairs.
+        if (testArray.length < 2) return;
+
+        /* 
+         * Value: The current int from `testArray`.
+         * Key:   The difference of that int and `targetSum`, needed to make a pair.
+         * 
+         * Note that the key and value are swapped. This is because Java HashMaps have many-to-one
+         * mappings, so it is easier to get values from keys than vice-versa, which is relevant
+         * during the iteration step.
+         * 
+         * In practice, this could be made clearer by using a custom or third-party one-to-one Map.
+         */
+        HashMap<Integer, Integer> pairs = new HashMap<>();
+
+        // Iterate through `testArray`.
+        for (int i : testArray) 
+        {
+            if (pairs.containsKey(i))
+            {
+                // If `i` was already needed (exists as a key), print the pair.
+                System.out.println("(" + i + ", " + pairs.get(i) + ")");
+            }
+            else
+            {
+                // Otherwise, calculate the difference and put in the potential pair.
+                pairs.put(targetSum - i, i);
+            }
+        }
+    }
+}
---
+++ algorithms/ReconcileArrays.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Question 2:
+ * 
+ * Reconcile Arrays.
+ * Given two unsorted integer arrays, print out the numbers that are NOT contained in the
+ * other array.
+ */
+
+class ReconcileArrays 
+{
+    public static void main(String[] args) 
+    {
+		// Test cases go here.
+	}
+
+    /**
+     * Approach:
+     * 
+	 * 1. Remove duplicates and cast both arrays to List to access useful methods.
+	 * 2. Remove all ints present in one List from the other for both Lists.
+	 * 3. Format and print the remaning ints.
+     */
+
+    static void reconcileHelper(int[] a, int[] b)
+    {
+        // Remove duplicates from both arrays and cast to List. (Stream: Java 8+)
+        List<Integer> aList = Arrays.stream(a).distinct().boxed().collect(Collectors.toList());
+        List<Integer> bList = Arrays.stream(b).distinct().boxed().collect(Collectors.toList());
+
+        // Clone either List for later use as the Lists will be modified in-place.
+        List<Integer> aCopy = new ArrayList<>(aList);
+
+        // Remove any shared ints from both arrays.
+        aList.removeAll(bList);
+        bList.removeAll(aCopy);
+
+        // Print any remaining ints in the prescribed format. (Text block literal: Java 15+)
+        System.out.println(String.format(
+            """
+            Numbers in array a that aren't in array b:
+            %s
+        
+            Numbers in array b that aren't in array a:
+            %s
+            """,
+            // Arrays are printed without commas or delimiting brackets.
+            aList.toString().replaceAll("[\\[,\\]]", ""),
+            bList.toString().replaceAll("[\\[,\\]]", "")
+        ));
+    }
+}
